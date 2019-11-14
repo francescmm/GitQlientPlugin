4dfd3c91534fb6005209d660fca8bed95c86b480
Francesc Martinez <francescmm@gmail.com>
2019-11-14 07:11:53 +0100

    Enabling OSX build

--- ci-scripts/osx/build.sh
+++ ci-scripts/osx/build.sh
@@ -1,7 +1,13 @@
-export PATH="/usr/local/opt/qt/bin:$PATH"
-source ~/.bash_profile
 export LDFLAGS="-L/usr/local/opt/qt/lib"
 export CPPFLAGS="-I/usr/local/opt/qt/include"
-qmake GitQlientPlugin.pro
+wget -O qtcreator-src.zip https://github.com/francescmm/ci-utils/releases/download/qt_5.13/qt-creator-opensource-src-4.10.0.zip;
+unzip -qq qtcreator-src.zip
+wget -O qtcreator-bin.zip https://github.com/francescmm/ci-utils/releases/download/qt_5.13/Tools.zip;
+unzip -qq qtcreator-bin.zip
+export QT_CREATOR=Tools/QtCreator/bin
+export PATH="/usr/local/opt/qt/bin:$QT_CREATOR:$PATH"
+export QT_PLUGIN_PATH=$PWD/qt5/plugins;
+source ~/.bash_profile
+qmake QTC_SOURCE=./qt-creator-opensource-src-4.10.0 QTC_BUILD=./Tools/QtCreator GitQlientPlugin.pro
 make
-ls -lsh
+ls -lsh Tools/QtCreator/lib/qtcreator/plugins/libGitQlientPlugin*

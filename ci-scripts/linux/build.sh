fbf82b3439beea21327a1875125ff1abbd577ebf
Francesc Martinez <francescmm@gmail.com>
2019-11-14 06:58:55 +0100

    Fixing QtCreator path

--- ci-scripts/linux/build.sh
+++ ci-scripts/linux/build.sh
@@ -5,7 +5,7 @@ unzip -qq qtcreator-src.zip
 wget -O qtcreator-bin.zip https://github.com/francescmm/ci-utils/releases/download/qt_5.13/Tools.zip;
 unzip -qq qtcreator-bin.zip
 export QTDIR=$PWD/qt5
-export QT_CREATOR=$HOME/Qt/Tools/QtCreator/bin
+export QT_CREATOR=Tools/QtCreator/bin
 export PATH=$QTDIR/bin:$QT_CREATOR:$PATH;
 export QT_PLUGIN_PATH=$PWD/qt5/plugins;
 $QTDIR/bin/qmake QTC_SOURCE=./qt-creator-opensource-src-4.10.0 QTC_BUILD=./Tools GitQlientPlugin.pro

99809e444bdc3fb10a857b22b292051a161d61d8
Francesc Martinez <francescmm@gmail.com>
2019-12-15 16:22:22 +0100

    Fixing versions on TravisCI

--- ci-scripts/linux/build.sh
+++ ci-scripts/linux/build.sh
@@ -1,6 +1,6 @@
 wget -O qt5.zip https://github.com/francescmm/ci-utils/releases/download/qt_5.13/qt5.zip;
 unzip -qq qt5.zip;
-wget -O qtcreator-src.zip https://github.com/francescmm/ci-utils/releases/download/qt_5.13/qt-creator-opensource-src-${QTC_VERSION}.zip;
+wget -O qtcreator-src.zip https://github.com/francescmm/ci-utils/releases/download/qt_5.13/qt-creator-opensource-src-${QTC_VERSION}.0.zip;
 unzip -qq qtcreator-src.zip
 wget -O qtcreator-bin.zip https://github.com/francescmm/ci-utils/releases/download/qt_5.13/qt-creator-bin-linux.zip;
 unzip -qq qtcreator-bin.zip
@@ -8,6 +8,7 @@ export QTDIR=$PWD/qt5
 export QT_CREATOR=Tools/QtCreator/bin
 export PATH=$QTDIR/bin:$QT_CREATOR:$PATH;
 export QT_PLUGIN_PATH=$PWD/qt5/plugins;
-$QTDIR/bin/qmake QTC_SOURCE=./qt-creator-opensource-src-${QTC_VERSION} QTC_BUILD=./Tools/QtCreator GitQlientPlugin.pro
+export PLUGIN_VERSION="1.0.1";
+$QTDIR/bin/qmake QTC_SOURCE=./qt-creator-opensource-src-${QTC_VERSION}.0 QTC_BUILD=./Tools/QtCreator GitQlientPlugin.pro
 make -j 4
-cp Tools/QtCreator/lib/qtcreator/plugins/libGitQlientPlugin.so libGitQlientPlugin${PLUGIN_VERSION}-qtc${QTC_COMPAT}.so
+cp Tools/QtCreator/lib/qtcreator/plugins/libGitQlientPlugin.so libGitQlientPlugin$PLUGIN_VERSION-qtc${QTC_COMPAT}.x.so
--- ci-scripts/osx/build.sh
+++ ci-scripts/osx/build.sh
@@ -1,13 +1,14 @@
 export LDFLAGS="-L/usr/local/opt/qt/lib"
 export CPPFLAGS="-I/usr/local/opt/qt/include"
-wget -O qtcreator-src.zip https://github.com/francescmm/ci-utils/releases/download/qt_5.13/qt-creator-opensource-src-${QTC_VERSION}.zip;
+wget -O qtcreator-src.zip https://github.com/francescmm/ci-utils/releases/download/qt_5.13/qt-creator-opensource-src-${QTC_VERSION}.0.zip;
 unzip -qq qtcreator-src.zip
 wget -O qtcreator-bin.zip https://github.com/francescmm/ci-utils/releases/download/qt_5.13/qt-creator-bin-osx.zip;
 unzip -qq qtcreator-bin.zip
 export QT_CREATOR=Tools/QtCreator/bin
 export PATH="/usr/local/opt/qt/bin:$QT_CREATOR:$PATH"
 export QT_PLUGIN_PATH=$PWD/qt5/plugins;
+export PLUGIN_VERSION="1.0.1";
 source ~/.bash_profile
-qmake QTC_SOURCE=./qt-creator-opensource-src-${QTC_VERSION} QTC_BUILD=. GitQlientPlugin.pro
+qmake QTC_SOURCE=./qt-creator-opensource-src-${QTC_VERSION}.0 QTC_BUILD=. GitQlientPlugin.pro
 make
-cp bin/Qt\ Creator.app/Contents/PlugIns/libGitQlientPlugin.dylib libGitQlientPlugin_${PLUGIN_VERSION}-qtc${QTC_COMPAT}.dylib
+cp bin/Qt\ Creator.app/Contents/PlugIns/libGitQlientPlugin.dylib libGitQlientPlugin_$PLUGIN_VERSION-qtc${QTC_COMPAT}.0.dylib

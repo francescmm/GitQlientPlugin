c404d457acd064fa2a1d5488f6f1507c0cd40f72
Francesc M <francesc.martinez@king.com>
2020-08-06 19:35:24 +0200

    Experimenting with OSX

--- ci-scripts/osx/build.sh
+++ ci-scripts/osx/build.sh
@@ -4,11 +4,13 @@ wget -O qtcreator-src.zip https://github.com/francescmm/ci-utils/releases/downlo
 unzip -qq qtcreator-src.zip
 #wget -O qtcreator-bin.zip https://github.com/francescmm/ci-utils/releases/download/qt/qt-creator-bin-osx.zip;
 #unzip -qq qtcreator-bin.zip
+ls /usr/local/Cellar/
+ls /usr/local/Cellar/qtcreator
 export QT_CREATOR=/Applications/Qt\ Creator.app/
 export PATH="/usr/local/opt/qt/bin:$QT_CREATOR:$PATH"
 export QT_PLUGIN_PATH=$PWD/qt5/plugins;
 export PLUGIN_VERSION="1.3.0";
 source ~/.bash_profile
-qmake QTC_SOURCE=./qt-creator-opensource-src-${QTC_VERSION}.0 QTC_BUILD=${QT_CREATOR} GitQlientPlugin.pro
-make
-cp bin/Qt\ Creator.app/Contents/PlugIns/libGitQlientPlugin.dylib libGitQlientPlugin_$PLUGIN_VERSION-qtc${QTC_COMPAT}.0.dylib
+#qmake QTC_SOURCE=./qt-creator-opensource-src-${QTC_VERSION}.0 QTC_BUILD=${QT_CREATOR} GitQlientPlugin.pro
+#make
+#cp bin/Qt\ Creator.app/Contents/PlugIns/libGitQlientPlugin.dylib libGitQlientPlugin_$PLUGIN_VERSION-qtc${QTC_COMPAT}.0.dylib
